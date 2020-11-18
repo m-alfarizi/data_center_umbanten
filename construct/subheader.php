a06e372dc921414edc7784f27fc55b10b84de883
Amir080915 <amirassyah1607@gmail.com>
2020-11-18 14:35:12 +0700

    merubah logo ke umbanten

--- construct/subheader.php
+++ construct/subheader.php
@@ -1,101 +1,99 @@
-
-            <!--begin::Wrapper-->
-            <div class="d-flex flex-column flex-row-fluid wrapper" id="kt_wrapper">
-                <!--begin::Header-->
-                <div id="kt_header" class="header  header-fixed ">
-                    <!--begin::Container-->
-                    <div class=" container-fluid  d-flex align-items-stretch justify-content-between">
-                        <!--begin::Header Menu Wrapper-->
-                        <div class="header-menu-wrapper header-menu-wrapper-left" id="kt_header_menu_wrapper">
-                            <!--begin::Header Menu-->
-                            <div id="kt_header_menu" class="header-menu header-menu-mobile  header-menu-layout-default ">
-                                <!--begin::Header Nav-->
-                                <div class="d-flex align-items-center mb-2">
-                                    <!--begin::logo-h-->
-                                    <div class="logo-h logo-h-40 mr-5">
-                                        <span class="logo-h-label">
-                                            <img src="assets/media/logos/logo_esdm.png" class="h-75" alt="">
-                                        </span>
-                                    </div>
-                                    <!--end::logo-h-->
-                                    <!--begin::Text-->
-                                    <div class="d-flex flex-column flex-grow-1 font-weight-bold">
-                                        <a href="#" class="text-dark text-hover-primary mb-1 font-size-lg">UNIVERSITAS MUHAMMADIYAH BANTEN</a>
-                                        <span class="text-muted">Jl. Aria Jaya Santika No.KM. 4 no. 40, Pasir Nangka, Tigaraksa Kec., Tangerang, Banten 15720</span>
-                                    </div>
-                                    <!--end::Text-->
-                                </div>
-                                <!--end::Header Nav-->
-                            </div>
-                            <!--end::Header Menu-->
+<!--begin::Wrapper-->
+<div class="d-flex flex-column flex-row-fluid wrapper" id="kt_wrapper">
+    <!--begin::Header-->
+    <div id="kt_header" class="header  header-fixed ">
+        <!--begin::Container-->
+        <div class=" container-fluid  d-flex align-items-stretch justify-content-between">
+            <!--begin::Header Menu Wrapper-->
+            <div class="header-menu-wrapper header-menu-wrapper-left" id="kt_header_menu_wrapper">
+                <!--begin::Header Menu-->
+                <div id="kt_header_menu" class="header-menu header-menu-mobile  header-menu-layout-default ">
+                    <!--begin::Header Nav-->
+                    <div class="d-flex align-items-center mb-2">
+                        <!--begin::logo-h-->
+                        <div class="logo-h logo-h-40 mr-5">
+                            <span class="logo-h-label">
+                                <img src="assets/media/logos/logo_umbanten.png" class="h-75" alt="">
+                            </span>
                         </div>
-                        <!--end::Header Menu Wrapper-->
-                        
-                        <!--begin::Topbar-->
-                        <div class="topbar">
-                            <!--begin::Notifications-->                            
-                            <div class="topbar-item" data-offset="10px,0px">
-                                <div class="btn btn-icon btn-clean btn-lg mr-1 pulse pulse-primary">
-                                    <span class="svg-icon svg-icon-xl svg-icon-primary">
-                                        <!--begin::Svg Icon | path:assets/media/svg/icons/Communication/Group-chat.svg--><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
-                                            <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                                                <rect x="0" y="0" width="24" height="24" />
-                                                <path d="M16,15.6315789 L16,12 C16,10.3431458 14.6568542,9 13,9 L6.16183229,9 L6.16183229,5.52631579 C6.16183229,4.13107011 7.29290239,3 8.68814808,3 L20.4776218,3 C21.8728674,3 23.0039375,4.13107011 23.0039375,5.52631579 L23.0039375,13.1052632 L23.0206157,17.786793 C23.0215995,18.0629336 22.7985408,18.2875874 22.5224001,18.2885711 C22.3891754,18.2890457 22.2612702,18.2363324 22.1670655,18.1421277 L19.6565168,15.6315789 L16,15.6315789 Z" fill="#000000" />
-                                                <path d="M1.98505595,18 L1.98505595,13 C1.98505595,11.8954305 2.88048645,11 3.98505595,11 L11.9850559,11 C13.0896254,11 13.9850559,11.8954305 13.9850559,13 L13.9850559,18 C13.9850559,19.1045695 13.0896254,20 11.9850559,20 L4.10078614,20 L2.85693427,21.1905292 C2.65744295,21.3814685 2.34093638,21.3745358 2.14999706,21.1750444 C2.06092565,21.0819836 2.01120804,20.958136 2.01120804,20.8293182 L2.01120804,18.32426 C1.99400175,18.2187196 1.98505595,18.1104045 1.98505595,18 Z M6.5,14 C6.22385763,14 6,14.2238576 6,14.5 C6,14.7761424 6.22385763,15 6.5,15 L11.5,15 C11.7761424,15 12,14.7761424 12,14.5 C12,14.2238576 11.7761424,14 11.5,14 L6.5,14 Z M9.5,16 C9.22385763,16 9,16.2238576 9,16.5 C9,16.7761424 9.22385763,17 9.5,17 L11.5,17 C11.7761424,17 12,16.7761424 12,16.5 C12,16.2238576 11.7761424,16 11.5,16 L9.5,16 Z" fill="#000000" opacity="0.3" />
-                                            </g>
-                                        </svg>
-                                        <!--end::Svg Icon--></span> <span class="pulse-ring"></span>
-                                </div>
-                            </div> 
-                            <!--end::Notifications-->                                                       
-                            <!--begin::Logout User-->
-                            <div class="topbar-item">
-                                <div class="btn btn-icon btn-icon-mobile w-auto btn-clean d-flex align-items-center btn-lg px-2" id="kt_quick_user_toggle">
-                                    <span class="text-muted font-weight-bold font-size-base d-none d-md-inline mr-1"></span>
-                                    <span class="text-dark-50 font-weight-bolder font-size-base d-none d-md-inline mr-3">Logout</span>
-                                    <span class="symbol symbol-lg-35 symbol-25 symbol-light-success">
-                                        <span class="symbol-label font-size-h5 font-weight-bold">S</span>
-                                    </span>
-                                </div>
-                            </div>
-                            <!--end::Logout User-->
+                        <!--end::logo-h-->
+                        <!--begin::Text-->
+                        <div class="d-flex flex-column flex-grow-1 font-weight-bold">
+                            <a href="#" class="text-dark text-hover-primary mb-1 font-size-lg">UNIVERSITAS MUHAMMADIYAH BANTEN</a>
+                            <span class="text-muted">Jl. Aria Jaya Santika No.KM. 4 no. 40, Pasir Nangka, Tigaraksa Kec., Tangerang, Banten 15720</span>
                         </div>
-                        <!--end::Topbar-->
+                        <!--end::Text-->
                     </div>
-                    <!--end::Container-->
+                    <!--end::Header Nav-->
                 </div>
-                <!--end::Header-->
-                <!--begin::Content-->
-                <div class="content  d-flex flex-column flex-column-fluid" id="kt_content">             	
-					<!--begin::Subheader-->
-                    <div class="subheader subheader-solid " id="kt_subheader">
-                        <div class=" container-fluid  d-flex align-items-center justify-content-between flex-wrap flex-sm-nowrap">
-                            <!--begin::Info-->
-                            <div class="d-flex align-items-center flex-wrap mr-2">
-                                <!--begin::Page Title-->
-                                <span class="text-dark font-weight-bold mt-2 mb-2 mr-5 font-size-sm">
-                                    Nama Pegawai
-									<br><span class="mb-0 d-block text-muted font-size-sm">1234567890 12345678</span>
-                                </span>
-                                <!--end::Page Title-->
-                                <!--begin::Action-->
-                                <div class="subheader-separator subheader-separator-ver mt-2 mb-2 mr-4 bg-gray-200"></div>
-                                <span class="text-muted font-size-sm mr-4">Inspektorat II</span>
-                				<!--a href="#" class="btn btn-light-primary font-weight-bolder btn-sm">Profil</a-->
-                                <!--end::Action-->
-                            </div>
-                            <!--end::Info-->
-                            <!--begin::Toolbar-->
-                            <div class="d-flex align-items-center flex-wrap">
-                                <!--begin::Daterange-->                                
-                                <a href="#" class="btn btn-bg-white font-weight-bold mr-3 my-2 my-lg-0" id="kt_dashboard_daterangepicker" data-toggle="tooltip" title="Select dashboard daterange" data-placement="left">
-                                    <span class="text-muted font-weight-bold mr-2" id="kt_dashboard_daterangepicker_title">Today</span>
-                                    <span class="text-primary font-weight-bolder" id="kt_dashboard_daterangepicker_date">Aug 16</span>
-                                </a>
-                                <!--end::Daterange-->
-                            </div>
-                            <!--end::Toolbar-->
-                        </div>
+                <!--end::Header Menu-->
+            </div>
+            <!--end::Header Menu Wrapper-->
+
+            <!--begin::Topbar-->
+            <div class="topbar">
+                <!--begin::Notifications-->
+                <div class="topbar-item" data-offset="10px,0px">
+                    <div class="btn btn-icon btn-clean btn-lg mr-1 pulse pulse-primary">
+                        <span class="svg-icon svg-icon-xl svg-icon-primary">
+                            <!--begin::Svg Icon | path:assets/media/svg/icons/Communication/Group-chat.svg--><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+                                <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                    <rect x="0" y="0" width="24" height="24" />
+                                    <path d="M16,15.6315789 L16,12 C16,10.3431458 14.6568542,9 13,9 L6.16183229,9 L6.16183229,5.52631579 C6.16183229,4.13107011 7.29290239,3 8.68814808,3 L20.4776218,3 C21.8728674,3 23.0039375,4.13107011 23.0039375,5.52631579 L23.0039375,13.1052632 L23.0206157,17.786793 C23.0215995,18.0629336 22.7985408,18.2875874 22.5224001,18.2885711 C22.3891754,18.2890457 22.2612702,18.2363324 22.1670655,18.1421277 L19.6565168,15.6315789 L16,15.6315789 Z" fill="#000000" />
+                                    <path d="M1.98505595,18 L1.98505595,13 C1.98505595,11.8954305 2.88048645,11 3.98505595,11 L11.9850559,11 C13.0896254,11 13.9850559,11.8954305 13.9850559,13 L13.9850559,18 C13.9850559,19.1045695 13.0896254,20 11.9850559,20 L4.10078614,20 L2.85693427,21.1905292 C2.65744295,21.3814685 2.34093638,21.3745358 2.14999706,21.1750444 C2.06092565,21.0819836 2.01120804,20.958136 2.01120804,20.8293182 L2.01120804,18.32426 C1.99400175,18.2187196 1.98505595,18.1104045 1.98505595,18 Z M6.5,14 C6.22385763,14 6,14.2238576 6,14.5 C6,14.7761424 6.22385763,15 6.5,15 L11.5,15 C11.7761424,15 12,14.7761424 12,14.5 C12,14.2238576 11.7761424,14 11.5,14 L6.5,14 Z M9.5,16 C9.22385763,16 9,16.2238576 9,16.5 C9,16.7761424 9.22385763,17 9.5,17 L11.5,17 C11.7761424,17 12,16.7761424 12,16.5 C12,16.2238576 11.7761424,16 11.5,16 L9.5,16 Z" fill="#000000" opacity="0.3" />
+                                </g>
+                            </svg>
+                            <!--end::Svg Icon--></span> <span class="pulse-ring"></span>
                     </div>
-                    <!--end::Subheader-->
-                    
+                </div>
+                <!--end::Notifications-->
+                <!--begin::Logout User-->
+                <div class="topbar-item">
+                    <div class="btn btn-icon btn-icon-mobile w-auto btn-clean d-flex align-items-center btn-lg px-2" id="kt_quick_user_toggle">
+                        <span class="text-muted font-weight-bold font-size-base d-none d-md-inline mr-1"></span>
+                        <span class="text-dark-50 font-weight-bolder font-size-base d-none d-md-inline mr-3">Logout</span>
+                        <span class="symbol symbol-lg-35 symbol-25 symbol-light-success">
+                            <span class="symbol-label font-size-h5 font-weight-bold">S</span>
+                        </span>
+                    </div>
+                </div>
+                <!--end::Logout User-->
+            </div>
+            <!--end::Topbar-->
+        </div>
+        <!--end::Container-->
+    </div>
+    <!--end::Header-->
+    <!--begin::Content-->
+    <div class="content  d-flex flex-column flex-column-fluid" id="kt_content">
+        <!--begin::Subheader-->
+        <div class="subheader subheader-solid " id="kt_subheader">
+            <div class=" container-fluid  d-flex align-items-center justify-content-between flex-wrap flex-sm-nowrap">
+                <!--begin::Info-->
+                <div class="d-flex align-items-center flex-wrap mr-2">
+                    <!--begin::Page Title-->
+                    <span class="text-dark font-weight-bold mt-2 mb-2 mr-5 font-size-sm">
+                        Nama Pegawai
+                        <br><span class="mb-0 d-block text-muted font-size-sm">1234567890 12345678</span>
+                    </span>
+                    <!--end::Page Title-->
+                    <!--begin::Action-->
+                    <div class="subheader-separator subheader-separator-ver mt-2 mb-2 mr-4 bg-gray-200"></div>
+                    <span class="text-muted font-size-sm mr-4">Inspektorat II</span>
+                    <!--a href="#" class="btn btn-light-primary font-weight-bolder btn-sm">Profil</a-->
+                    <!--end::Action-->
+                </div>
+                <!--end::Info-->
+                <!--begin::Toolbar-->
+                <div class="d-flex align-items-center flex-wrap">
+                    <!--begin::Daterange-->
+                    <a href="#" class="btn btn-bg-white font-weight-bold mr-3 my-2 my-lg-0" id="kt_dashboard_daterangepicker" data-toggle="tooltip" title="Select dashboard daterange" data-placement="left">
+                        <span class="text-muted font-weight-bold mr-2" id="kt_dashboard_daterangepicker_title">Today</span>
+                        <span class="text-primary font-weight-bolder" id="kt_dashboard_daterangepicker_date">Aug 16</span>
+                    </a>
+                    <!--end::Daterange-->
+                </div>
+                <!--end::Toolbar-->
+            </div>
+        </div>
+        <!--end::Subheader-->
